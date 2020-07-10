eb3ebc39dab47ebe7a3a814a2656e22e8f99c0f9
Leandro <leandroglez39@gmail.com>
2020-07-10 12:40:17 -0400

    Inicial commit

---
+++ CSV.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Tester
+{
+    public class CSV
+    {
+        public int ID { get; set; }
+        public string STATE { get; set; }
+        public string TASK { get; set; }
+        public string TIME { get; set; }
+        public string BOSS { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+
+namespace Tester
+{
+    class Program
+    {
+        private static void Main()
+        {
+
+            var reader = new StreamReader("C:\\Users\\Leo\\Desktop\\TraceAligment CSV Data\\CargaBDClassLibraryOK\\ConsoleApp1\\bin\\Debug\\EVENTO.csv");
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<CSV>().ToList();
+
+            foreach (var x in records)
+                Console.WriteLine(x.BOSS);
+            Console.WriteLine();
+
+        }
+    }
+}
